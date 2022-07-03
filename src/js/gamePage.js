1946ea0d15f4cdc481963cbc7d1f27df30bc5c01
Serhii Humenchuk <serhii.humenchuk-external@sema4genomics.com>
2022-07-03 09:05:00 +0300

    Added header content

--- src/js/gamePage.js
+++ src/js/gamePage.js
@@ -1,9 +1,27 @@
 import { roulette } from './roulette';
 
 export const gamePage = () => {
+    console.log('hi')
     const rootElement = document.getElementById('root');
-    const header = document.createElement('header');
-    header.classList.add('header');
-    rootElement.append(header);
+    const headerElement = document.createElement('header');
+    headerElement.classList.add('header');
+    console.log(headerElement)
+
+    const gamesListElement = document.createElement('ul');
+        gamesListElement.classList.add('gamesList');
+    console.log(gamesListElement)
+
+    const games = ['Poker', 'Blackjack', 'Roulette', 'Baccarat', 'Craps', 'Slots'];
+    games.map(game => {
+        const gameItemElement = document.createElement('li');
+        gameItemElement.classList.add('gameItem');
+        if(game === 'Roulette') gameItemElement.classList.add('rouletteGame');
+        gameItemElement.textContent = game;
+        console.log(gameItemElement)
+        gamesListElement.append(gameItemElement);
+    })
+
+    headerElement.append(gamesListElement);
+    rootElement.append(headerElement);
     roulette();
 }
