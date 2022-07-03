4d4a9a398d1ab29f3a0e9c9ba659107b7a1d2126
Serhii Humenchuk <serhii.humenchuk-external@sema4genomics.com>
2022-07-03 09:30:05 +0300

    added fiish game button

--- src/js/gamePage.js
+++ src/js/gamePage.js
@@ -1,27 +1,49 @@
 import { roulette } from './roulette';
+import {re} from "../../dist/index.975ef6c8";
 
-export const gamePage = () => {
-    console.log('hi')
-    const rootElement = document.getElementById('root');
-    const headerElement = document.createElement('header');
-    headerElement.classList.add('header');
-    console.log(headerElement)
-
+const renderGamesList = () => {
     const gamesListElement = document.createElement('ul');
-        gamesListElement.classList.add('gamesList');
-    console.log(gamesListElement)
-
+    gamesListElement.classList.add('gamesList');
     const games = ['Poker', 'Blackjack', 'Roulette', 'Baccarat', 'Craps', 'Slots'];
     games.map(game => {
         const gameItemElement = document.createElement('li');
         gameItemElement.classList.add('gameItem');
         if(game === 'Roulette') gameItemElement.classList.add('rouletteGame');
         gameItemElement.textContent = game;
-        console.log(gameItemElement)
         gamesListElement.append(gameItemElement);
     })
 
+    return gamesListElement
+};
+
+
+const renderFinishGameButton = () => {
+    const finishGameBtnElement = document.createElement('button');
+    finishGameBtnElement.textContent = 'Finish Game';
+    finishGameBtnElement.classList.add('finishGameBtn');
+
+    const handleFinishGame = () => {
+        alert('Game Finished')
+    }
+
+    finishGameBtnElement.addEventListener('click', handleFinishGame)
+
+    return finishGameBtnElement;
+}
+
+
+export const gamePage = () => {
+    const rootElement = document.getElementById('root');
+    const headerElement = document.createElement('header');
+    headerElement.classList.add('header');
+
+    const gamesListElement = renderGamesList();
     headerElement.append(gamesListElement);
+
+    const finishGameBtnElement = renderFinishGameButton()
+    headerElement.append(finishGameBtnElement);
+
+
     rootElement.append(headerElement);
     roulette();
 }
